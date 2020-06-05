f2e5ac272598c28aa27f361df0941c02d4ba1def
JAnanthakrishnan <ananthakrishnan2017@gmail.com>
2020-06-05 22:17:34 +0530

    express-validator setup

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const connectDB = require("./config/db");
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 //connect DB
 connectDB();
-app.get("/", (req, res) => res.send("API Running ..."));
-app.use("/api/users", require("./routes/api/users"));
-app.use("/api/auth", require("./routes/api/auth"));
-app.use("/api/posts", require("./routes/api/posts"));
-app.use("/api/profile", require("./routes/api/profile"));
+app.use(express.json({ extended: false }));
+app.get('/', (req, res) => res.send('API Running ...'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);
