28cf27f9a5708f542df3196adde2015936ca4157
JAnanthakrishnan <ananthakrishnan2017@gmail.com>
2020-06-05 09:48:09 +0530

    established end points

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 //connect DB
 connectDB();
 app.get("/", (req, res) => res.send("API Running ..."));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);
