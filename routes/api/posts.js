0c90d279f4e14cfc12dfbb937102a73d1a6326fb
J Ananthakrishnan <ananthakrishnan2017@gmail.com>
2020-06-24 22:38:32 +0530

    working to get a single post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const auth = require('../../middleware/auth');
-const Post = require('../../models/Posts');
-const User = require('../../models/User');
+const auth = require("../../middleware/auth");
+const Post = require("../../models/Posts");
+const User = require("../../models/User");
 const {
   check,
   expressValidator,
-  validationResult
-} = require('express-validator');
+  validationResult,
+} = require("express-validator");
 
 /**
  * *@route  POST api/posts
@@ -15,26 +15,26 @@ const {
  * *@access Private
  */
 router.get(
-  '/',
-  [auth, [check('text', 'Text is required').not().isEmpty()]],
+  "/",
+  [auth, [check("text", "Text is required").not().isEmpty()]],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
     try {
-      const user = await User.findById(req.user.id).select('-password');
+      const user = await User.findById(req.user.id).select("-password");
       const newPost = new Post({
         text: req.body.text,
         name: user.name,
         avatar: user.avatar,
-        user: req.user.id
+        user: req.user.id,
       });
       const post = newPost.save();
       res.json(post);
     } catch (err) {
       console.error(err.message);
-      res.status(500).send('Server Error');
+      res.status(500).send("Server Error");
     }
   }
 );
@@ -43,14 +43,19 @@ router.get(
  * *@desc get all the posts
  * *@access Private
  */
-router.get('/', auth, async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
     const posts = await Post.find().sort({ date: -1 });
     res.json(posts);
   } catch (err) {
     console.error(err.message);
-    res.status(500).send('Server Error');
+    res.status(500).send("Server Error");
   }
 });
-
+/**
+ * *@route  GET api/posts/:post_id
+ * *@desc get a single post
+ * *@access Private
+ */
+router.get("/", auth, async (req, res) => {});
 module.exports = router;
